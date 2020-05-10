ab2b84c11db14bc65420b3975199a5474ab99e53
moises-dias <moisesdepaulodias@gmail.com>
2020-05-10 11:58:16 -0300

    creating android app and fixing authentication

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from './auth.service';
+import { AuthService, AuthResponseData } from './auth.service';
 import { Router } from '@angular/router';
-import { LoadingController } from '@ionic/angular';
+import { LoadingController, AlertController } from '@ionic/angular';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -18,23 +19,42 @@ export class AuthPage implements OnInit {
   constructor(
     private authService: AuthService,
     private router: Router,
-    private loadingCtrl: LoadingController
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController
   ) { }
 
   ngOnInit() {}
 
-  onLogin() {
+  authenticate(email: string, password: string) {
     this.isLoading = true;
-    this.authService.login();
     this.loadingCtrl
       .create({ keyboardClose: true, message: 'Logging in...' })
       .then(loadingEl => {
         loadingEl.present();
-        setTimeout(() => {
+        let authObs: Observable<AuthResponseData>;
+        if (this.isLogin) {
+          authObs = this.authService.login(email, password);
+        } else {
+          authObs = this.authService.signup(email, password);
+        }
+        authObs.subscribe(resData => {
+          console.log(resData);
           this.isLoading = false;
           loadingEl.dismiss();
           this.router.navigateByUrl('/home');
-        }, 1500);
+        }, errRes => {
+          loadingEl.dismiss();
+          const code = errRes.error.error.message;
+          let message = 'Could not sign you up, please try again.'
+          if (code === 'EMAIL_EXISTS') {
+            message = "This email address exists already!"
+          } else if (code === 'EMAIL_NOT_FOUND') {
+            message = 'E-Mail address could not be found.'
+          } else if (code === 'INVALID_PASSWORD') {
+            message = 'This password is not correct.'
+          }
+          this.showAlert(message);
+        });
       });
   }
 
@@ -49,14 +69,17 @@ export class AuthPage implements OnInit {
     const email = form.value.email;
     const password = form.value.password;
     // console.log(email, password);
+    this.authenticate(email, password);
+  }
 
-    if (this.isLogin) {
-      // Send a request to login servers
-    } else {
-      this.authService.signup(email, password).subscribe(resData => {
-        console.log(resData);
+  private showAlert(message: string) {
+    this.alertCtrl
+      .create({
+        header: 'Authentication failed', 
+        message: message, 
+        buttons: ['Okay']
       })
-    }
+      .then(alertEl => alertEl.present());
   }
 
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { BehaviorSubject } from 'rxjs';
+import { User } from './user.model';
+import { map, tap } from 'rxjs/operators';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string;
@@ -19,29 +22,59 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-  private _userIsAuthenticated = false;
-  private _userId = null;
+  private _user = new BehaviorSubject<User>(null);
 
   get userIsAuthenticated() {
-    return this._userIsAuthenticated;
+    return this._user.asObservable().pipe(
+      map(user => {
+        if (user) {
+          return !!user.token;
+        } else {
+          return false;
+        }
+      })
+    );
   }
 
   get userId() {
-    return this._userId;
+    return this._user.asObservable().pipe(
+      map(user => { 
+        if (user) {
+          return user.id;
+        } else {
+          return null;
+        }
+      })
+    );
   }
 
-  login() {
-    this._userIsAuthenticated = true;
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseAPIKey}`,
+      { email: email, password: password }
+    ).pipe(tap(this.setUserData.bind(this)));
   }
 
   logout() {
-    this._userIsAuthenticated = false;
+    this._user.next(null);
   }
 
   signup(email: string, password: string) {
     return this.http.post<AuthResponseData>(
       `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseAPIKey}`, 
       { email: email, password: password, returnSecureToken: true}
-    )
+    ).pipe(tap(this.setUserData.bind(this)));
+  }
+
+  private setUserData(userData: AuthResponseData) {
+    const expirationTime = new Date(new Date().getTime() + (+userData.expiresIn * 1000));
+    this._user.next(
+      new User(
+        userData.localId, 
+        userData.email, 
+        userData.idToken, 
+        expirationTime
+      )
+    );
   }
 }
